31c4ab8eb19a16198c633567379d150630ef57b2
Luis Miguel Jaime <luism.jaimeh@gmail.com>
2022-02-12 15:28:02 -0500

    added react router, sass, bootstrap CDN, made basic folders and architecture as for now

---
+++ src/ClassMates.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { NavBar } from './components/ui/NavBar'
+
+export const ClassMates  = () => {
+  return (
+    <NavBar/>
+  )
+};
---
+++ src/components/ui/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NavBar = () => {
+  return (
+    <div>
+        <h1>Yo</h1>
+    </div>
+  )
+}
