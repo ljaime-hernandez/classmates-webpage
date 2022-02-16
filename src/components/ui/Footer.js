fd9d9d69bb69ac81be62bb1efdf455c225f0bf7a
Luis Miguel Jaime <luism.jaimeh@gmail.com>
2022-02-16 17:48:52 -0500

    fixed href tag

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -8,11 +8,11 @@ export const Footer = () => {
   <div className="col">
       <h5>Section</h5>
       <ul className="nav flex-column">
-        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
-        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
-        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
-        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+        <li className="nav-item mb-2"><a href="home" className="nav-link p-0 text-muted">Home</a></li>
+        <li className="nav-item mb-2"><a href="home" className="nav-link p-0 text-muted">Features</a></li>
+        <li className="nav-item mb-2"><a href="home" className="nav-link p-0 text-muted">Pricing</a></li>
+        <li className="nav-item mb-2"><a href="home" className="nav-link p-0 text-muted">FAQs</a></li>
+        <li className="nav-item mb-2"><a href="home" className="nav-link p-0 text-muted">About</a></li>
       </ul>
     </div>
 
@@ -21,8 +21,6 @@ export const Footer = () => {
       <a href="https://www.facebook.com/classmatesclub.english"><i className="fa-brands fa-facebook fa-3x me-2"></i></a>
       <a href="https://www.instagram.com/classmateslessons"><i className="fa-brands fa-instagram fa-3x me-2"></i></a>
     </div>
-
-    
 
   </footer>
 </div>
