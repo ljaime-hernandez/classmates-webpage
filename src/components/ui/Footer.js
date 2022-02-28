590005b0b71ad339b9d892775fd9bd2d9a03dc54
Luis Miguel Jaime <luism.jaimeh@gmail.com>
2022-02-28 15:39:41 -0500

    icon fix WIP

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -4,11 +4,12 @@ import { Link } from 'react-router-dom'
 export const Footer = () => {
   return (
     <div className="container">
-  <footer className="row row-cols-5 py-5 border-top footer__navigation">
+      <footer className="row row-cols-5 py-5 border-top footer__navigation">
 
-  <div className="col">
-      <h5>Section</h5>
-      <ul className="nav flex-column">
+      <div className="col">
+        <div className='mt-3 mb-2 navbar__icon'></div>
+          <h3>ClassMates</h3>
+          <ul className="nav flex-column">
 
             <Link 
                 to="/"
@@ -34,24 +35,24 @@ export const Footer = () => {
                       <li className="nav-item mb-2 nav-link p-0 text-muted">Contact</li>
             </Link>
 
-      </ul>
-    </div>
+          </ul>
+      </div>
 
-    <div className="col">
+      <div className="col">
         <p className="text-muted mb-3">Follow us</p>
-      <a href="https://www.facebook.com/classmatesclub.english"><i className="fa-brands fa-facebook fa-3x me-2"></i></a>
-      <a href="https://www.instagram.com/classmateslessons"><i className="fa-brands fa-instagram fa-3x me-2"></i></a>
-      <a href="https://wa.me/16475630998?text=Me%20interesan%20las%20clases%20de%20ingles"><i className="fa-brands fa-whatsapp fa-3x"></i></a>
-    </div>
-  </footer>
+        <a href="https://www.facebook.com/classmatesclub.english"><i className="fa-brands fa-facebook fa-3x me-2"></i></a>
+        <a href="https://www.instagram.com/classmateslessons"><i className="fa-brands fa-instagram fa-3x me-2"></i></a>
+        <a href="https://wa.me/573223348472?text=Me%20interesan%20las%20clases%20de%20ingles"><i className="fa-brands fa-whatsapp fa-3x"></i></a>
+      </div>
 
-  <div className='col footer__whatsapp'>
-      <a href="https://wa.me/16475630998?text=Me%20interesan%20las%20clases%20de%20ingles">
+      </footer>
+
+      <div className='col footer__whatsapp'>
+        <a href="https://wa.me/573223348472?text=Me%20interesan%20las%20clases%20de%20ingles">
           <i className="fa-brands fa-whatsapp fa-3x"></i>
-      </a>
-  </div>
+        </a>
+      </div>
 
-</div>
-
+    </div>
   )
 }
